58b8238804935b7dbc2d53e9a1c7470af485f4c9
Mateus David de Souza <73516308+mtds20@users.noreply.github.com>
2025-11-25 16:35:12 +0000

    Deploying to vercel

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <h1>
+      Portfolio:{" "}
+      <a 
+        href="https://mtds20.github.io/" 
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        https://mtds20.github.io
+      </a>
+    </h1>
+  );
+}
+
+
+export default Home;
